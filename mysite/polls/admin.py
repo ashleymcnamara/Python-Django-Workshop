41148efdb9533b0ad2b06df285dc60bac175db89
Colin McNamara <colin@2cups.com>
2014-03-08 12:35:41 -0800

    added colapsing fields

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,8 +3,8 @@ from polls.models import Poll
 
 class PollAdmin(admin.ModelAdmin):
 	fieldsets = [
-		(None, {'fields': ['question']}),
-		('Date information', {'fields': ['pub_date']}),
+		(None, 					{'fields': ['question']}),
+		('Date information', 	{'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
 
 admin.site.register(Poll, PollAdmin)
