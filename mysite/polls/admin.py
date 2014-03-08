696eddd8f267d897cdbf4cdebb65c4a9c6ccf4b4
Colin McNamara <colin@2cups.com>
2014-03-08 12:59:28 -0800

    added search fields and filters

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from polls.models import Choice, Poll
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
 		model = Choice
 		extra = 3
 
@@ -11,6 +11,9 @@ class PollAdmin(admin.ModelAdmin):
 		('Date information', 	{'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
 	inlines = [ChoiceInline]
+	list_display = ('question', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question']
 
 admin.site.register(Poll, PollAdmin)
 
