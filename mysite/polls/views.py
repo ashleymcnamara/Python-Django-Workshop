5a22a71e0595dfc16bccf9055193cf754876d483
Colin McNamara <colin@2cups.com>
2014-03-09 12:35:10 -0700

    4.2

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,11 @@
 
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from polls.models import Choice, Poll
 from django.template import RequestContext, loader
-from django.shortcuts import render, get_object_or_404
+
+
 
 from polls.models import Poll
 
@@ -19,6 +23,27 @@ def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-	return HttpResponse("You're voting on poll %s." % poll_id)
+	# pk stands for primary key
+	# foreign key is a logical representation of primary keys in multiple data models
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+		# Request.POST lets you access data by key name
+		# Request.POST['choice'] raises KeyError if the choice wasn't provided
+		# 
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the poll voting form.
+		return render(request, 'polls/detail.html', {
+			'poll': p,
+			'error_message': "You didn't select a choice.",
+		})
+		else:
+			selected_choice.votes += 1
+			selected_choice.save()
+			# Always return an HttpResponseRedirect after successfully dealing
+			# with POST data. This prevents data from being posted twice if a 
+			# user hits the Back button.
+			return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
 
 # Create your views here.
