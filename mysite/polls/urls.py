f69c9521b398a64257a392cddb5b680060d91eb3
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-03-08 16:20:32 -0800

    I unfucked everything.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+
 from polls import views
-from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index')
 )
 
 
