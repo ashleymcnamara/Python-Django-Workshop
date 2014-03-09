01af41964a8739ffff330d4751cc2cd0d7d73a89
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-03-08 16:53:00 -0800

    fixed root url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from polls import views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    #The idea behind include() is to make 
+    # it easy to plug-and-play URLs
+    url(r'^$', views.index, name='index'),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
