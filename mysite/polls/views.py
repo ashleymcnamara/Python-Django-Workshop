a342356ab61f9649174a81a02f7a01d0941a1402
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-03-09 11:08:11 -0700

    added detail.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 
-from django.http import HttpResponse
+from django.http import Http404
 from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404
 
@@ -11,11 +11,12 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})
+	poll = get_object_or_404(Poll, pk=poll_id)
+	return render(request, 'polls/detail.html', {'poll': poll})
+
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+	return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
 	return HttpResponse("You're voting on poll %s." % poll_id)
