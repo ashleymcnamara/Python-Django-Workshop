5fc603ac561a98e5a293fc1b368587474d30d530
Ashley McNamara <ashleymcnamara1@gmail.com>
2014-03-09 18:04:04 -0700

    fixed errors in test.py

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -7,67 +7,60 @@ from polls.models import Poll
 
 class PollMethodTests(TestCase):
 
-	def test_was_published_recently_with_future_poll(self):
-		"""
-		was_published_recently() should return False for polls whose
-		pub_date is in the future
-		"""
-		future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
-		self.assertEqual(future_poll.was_published_recently(), False)
-
+    def test_was_published_recently_with_future_poll(self):
+        """
+        was_published_recently() should return False for polls whose
+        pub_date is in the future
+        """
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
 # What we have done here is created a django.test.TestCase subclass 
 # with a method that creates a Poll instance with a pub_date in the 
 # future. We then check the output of was_published_recently() - 
 # which ought to be False.
 
 	def test_was_published_recently_with_old_poll(self):
-		"""
-		was_published_recently() should return False for polls whose
-		pub_date is older than 1 days
-		"""
 		old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
 		self.assertEqual(old_poll.was_published_recently(), False)
 
 	def test_was_published_recently_with_recent_poll(self):
-		"""
-		was_published_recently() should return True for polls whose pub_date
-		is within the last day
-		"""
 		recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
 		self.assertEqual(recent_poll.was_published_recently(), True)
 
-	def create_poll(question, days):
-		 """
-		Creates a poll with the given `question` published the given number of
-		`days` offset to now (negative for polls published in the past,
-		positive for polls that have yet to be published).
-		"""
-	return Poll.objects.create(question=question,
-			pub_date=timezone.now() + datetime.timedelta(days=days))
+
+def create_poll(question, days):
+    """
+    Creates a poll with the given `question` published the given number of
+    `days` offset to now (negative for polls published in the past,
+    positive for polls that have yet to be published).
+    """
+    return Poll.objects.create(question=question,
+        pub_date=timezone.now() + datetime.timedelta(days=days))
 
 class PollViewTests(TestCase):
-	def test_index_view_with_no_polls(self):
-		"""
-		If no polls exist, an appropriate message should be displayed.
-		"""
-		response = self.client.get(reverse('polls:index'))
-		self.assertEqual(response.status_code, 200)
-		self.assertContains(response, "No polls are available.")
-		self.assertQuerysetEqual(response.context['latest_poll_list'], [])
 
-	def test_index_view_with_a_past_poll(self):
-		"""
+    def test_index_view_with_no_polls(self):
+        """
+        If no polls exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_poll_list'], [])
+
+    def test_index_view_with_a_past_poll(self):
+        """
         Polls with a pub_date in the past should be displayed on the index page.
         """
         create_poll(question="Past poll.", days=-30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
-        	response.context['latest_poll_list'],
-        	['<Poll: Past poll.>']
-		)
+            response.context['latest_poll_list'],
+            ['<Poll: Past poll.>']
+        )
 
-	def test_index_view_with_a_future_poll(self):
-		"""
+    def test_index_view_with_a_future_poll(self):
+        """
         Polls with a pub_date in the future should not be displayed on the
         index page.
         """
@@ -75,9 +68,9 @@ class PollViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.", status_code=200)
         self.assertQuerysetEqual(response.context['latest_poll_list'], [])
-	
-	def test_index_view_with_future_poll_and_past_poll(self):
-		"""
+
+    def test_index_view_with_future_poll_and_past_poll(self):
+        """
         Even if both past and future polls exist, only past polls should be
         displayed.
         """
@@ -85,70 +78,41 @@ class PollViewTests(TestCase):
         create_poll(question="Future poll.", days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
-        	response.context['latest_poll_list'],
-        	['<Poll: Past poll.>']
+            response.context['latest_poll_list'],
+            ['<Poll: Past poll.>']
         )
-	def test_index_view_with_two_past_polls(self):
-		"""
+
+    def test_index_view_with_two_past_polls(self):
+        """
         The polls index page may display multiple polls.
         """
         create_poll(question="Past poll 1.", days=-30)
         create_poll(question="Past poll 2.", days=-5)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
-        	response.context['latest_poll_list'],
-        	['<Poll: Past poll 2.>', '<Poll: Past poll 1.>']
-        ) 
+            response.context['latest_poll_list'],
+             ['<Poll: Past poll 2.>', '<Poll: Past poll 1.>']
+        )
 
+class PollIndexDetailTests(TestCase):
 
+    def test_detail_view_with_a_future_poll(self):
+        """
+        The detail view of a poll with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_poll = create_poll(question='Future poll.', days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_poll.id,)))
+        self.assertEqual(response.status_code, 404)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def test_detail_view_with_a_past_poll(self):
+        """
+        The detail view of a poll with a pub_date in the past should display
+        the poll's question.
+        """
+        past_poll = create_poll(question='Past Poll.', days=-5)
+        response = self.client.get(reverse('polls:detail', args=(past_poll.id,)))
+        self.assertContains(response, past_poll.question, status_code=200)
 
 
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,20 +9,20 @@ from django.utils import timezone
 from polls.models import Choice, Poll
 
 class IndexView(generic.ListView):
-	template_name = 'polls/index.html'
-	context_object_name = 'latest_poll_list'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
 
-	def get_queryset(self):
-		""" Return the last five published polls. (not including
-			those to be published in the future)
-		"""
-		return Poll.objects.filter(
-			pub_date__lte=timezone.now()
-		).order_by('-pub_date')[:5]
+    def get_queryset(self):
+    	return Poll.objects.filter(
+    		pub_date__lte=timezone.now()
+    		).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Poll
 	template_name = 'polls/detail.html'
+	def get_queryset(self):
+		return Poll.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
 	model = Poll
